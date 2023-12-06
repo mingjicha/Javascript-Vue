9d7a6fd19780d21c3adbedb281dfd778e643b738
mingjicha <mingi9100@naver.com>
2023-12-06 22:07:52 +0900

    1206 vue style

---
+++ vue_gym/learn-vue3/src/lectures/9/App_style_binding.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<!-- style도 class처럼 객체나 배열로 바인딩 할 수 있음 -->
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores neque ea
+			numquam ullam quos a, soluta praesentium, possimus cum dolore voluptas
+			quod, cumque eveniet saepe! Atque, dolor! Assumenda, perferendis
+			perspiciatis.
+		</div>
+		<button v-on:click="fontSize--">-</button>
+		<button v-on:click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+		const fontSize = ref(13);
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return {
+			styleObject,
+			fontSize,
+		};
+	},
+};
+// 😓와 여기서 에러 생겼음... fontsize라고 적어서 에러난 것. fontSize로 표기 해야 에러가 수정됨...
+</script>
+
+<style lang="scss" scoped></style>
