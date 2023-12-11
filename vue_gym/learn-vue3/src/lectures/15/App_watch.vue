96df77348f1efe6562b4523187741d1247eae7c6
mingjicha <mingi9100@naver.com>
2023-12-11 22:12:40 +0900

    1211 watch 어려웡..

---
+++ vue_gym/learn-vue3/src/lectures/15/App_watch.vue
@@ -0,0 +1,27 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+	setup() {
+		const message = ref('Hello World');
+		// DOM ~
+
+		// API ~
+		// state 변경
+
+		// message 데이터 변경에
+		watch(message, (newValue, oldValue) => {
+			console.log('newValue: ', newValue);
+			console.log('oldValue: ', oldValue);
+			// 어떠한 작업을 수행하는 "감시자" 역할을 합니다.
+		});
+		return { message };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
