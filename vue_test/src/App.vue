cee2f7673f09a613c73565398e4ed11bc90ad490
mingjicha <mingi9100@naver.com>
2023-12-13 04:32:35 +0900

    1212 css조금..

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,47 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<div>
+		<div>
+			<header class="header">헤더 띄우기</header>
+		</div>
+		<div class="main">
+			<div class="main_1">
+				<img class="main_sl" src="./assets/maru.png" alt="메인이미지" />
+				<div class="main_txt">
+					놀러오세요! 경상도, 좋아요!<br />블루베리스무디 맛있어요.<br />이의이승이
+					뭐냐구요 ?<br />일단, 와보면 알아요!
+				</div>
+				<div class="main_wea_up">날씨 위</div>
+				<div class="main_wea_down">날씨 아래</div>
+			</div>
+			<div class="main_2">메인2</div>
+			<div class="main_3">메인3</div>
+			<div class="main_4">메인4</div>
+			<div class="main_5">메인5</div>
+		</div>
+		<div>
+			<footer class="footer">푸터 띄우기</footer>
+		</div>
+	</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
