984e3f8ddbb74219b2090b58d518f74a88056af9
mingjicha <mingi9100@naver.com>
2023-12-13 14:05:31 +0900

    1213 2차 추가

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,8 +1,66 @@
-User
 <template>
 	<div>
 		<div>
-			<header class="header">헤더 띄우기 홈 | 경상도 | 메뉴</header>
+			<header class="header">
+				<div class="header_left">
+					<h1 class="logo"><a href="#">안냥</a></h1>
+				</div>
+				<div class="header_mid">
+					<nav class="lnb">
+						<ul class="nav">
+							<li>
+								<a href="#" target="_self">홈이야요</a>
+								<ul class="depth1">
+									<li>
+										<a href="#" target="_self" onclick="">블루베리</a>
+									</li>
+									<li>
+										<a href="#" target="_self" onclick="">스우므디</a>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="#" target="_self">경상도오</a>
+								<ul class="depth1">
+									<li>
+										<a href="#" target="_self" onclick="">경상남도구</a>
+									</li>
+									<li>
+										<a href="#" target="_self" onclick="">경상북돈디</a>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="#" target="_self">배고프당</a>
+								<ul class="depth1">
+									<li>
+										<a href="#" target="_self" onclick="">마라아탕</a>
+									</li>
+									<li>
+										<a href="#" target="_self" onclick="">먹구시펑</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</nav>
+				</div>
+				<div class="header_right">
+					<ul class="gnb">
+						<li>
+							<a href="#">LOGIN</a>
+						</li>
+						<li>
+							<a href="#">JOIN</a>
+						</li>
+					</ul>
+				</div>
+			</header>
+			<a href="javascript:void(0);" class="menu_btn">
+				<span></span>
+				<span></span>
+				<span></span>
+				<i class="sound_only">메뉴</i>
+			</a>
 		</div>
 		<div class="main">
 			<div class="main_1">
@@ -47,6 +105,7 @@ User
 			<footer class="footer">푸터 띄우기</footer>
 		</div>
 	</div>
+	<div class="main_topBtn"></div>
 </template>
 
 <script></script>
