51e4593f1392d6ebc03c6c9e7e3607d20b42e929
mingjicha <mingi9100@naver.com>
2023-12-24 19:30:51 +0900

    1224 헤더 테스트용

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,313 +1,107 @@
 <template>
-	<div>
-		<header class="header">
-			<div class="header_left">
-				<h1 class="logo"><a href="#">안냥</a></h1>
-			</div>
-			<div class="header_mid">
-				<nav class="lnb">
-					<ul class="nav">
-						<li>
-							<a href="#" target="_self">홈이야요</a>
-							<ul class="depth1">
-								<li>
-									<a href="#" target="_self" onclick="">블루베리</a>
-								</li>
-								<li>
-									<a href="#" target="_self" onclick="">스우므디</a>
-								</li>
-							</ul>
-						</li>
-						<li>
-							<a href="#" target="_self">경상도오</a>
-							<ul class="depth1">
-								<li>
-									<a href="#" target="_self" onclick="">경상남도구</a>
-								</li>
-								<li>
-									<a href="#" target="_self" onclick="">경상북돈디</a>
-								</li>
-							</ul>
-						</li>
-						<li>
-							<a href="#" target="_self">배고프당</a>
-							<ul class="depth1">
-								<li>
-									<a href="#" target="_self" onclick="">마아라탕</a>
-								</li>
-								<li>
-									<a href="#" target="_self" onclick="">먹구시펑</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</nav>
-			</div>
-			<div class="header_right">
-				<ul class="gnb">
+	<header class="header">
+		<div class="header_left">
+			<a href="#" class="logo">안냥</a>
+		</div>
+		<div class="header_mid">
+			<nav class="lnb">
+				<ul class="nav">
 					<li>
-						<a href="#">로그인</a>
-					</li>
-					<li>
-						<a href="#">회원가입</a>
-					</li>
-				</ul>
-			</div>
-		</header>
-		<a href="javascript:void(0);" class="menu_btn">
-			<span></span>
-			<span></span>
-			<span></span>
-			<i class="sound_only">메뉴</i>
-		</a>
-	</div>
-	<div>
-		<div class="main">
-			<div class="main_1">
-				<div class="main_1_box">
-					<div class="main_txt">
-						<div
-							class="main_txt_up animate__animated animate__lightSpeedInRight"
-						>
-							놀러오세요! 경상도, 좋아요!<br />
-						</div>
-						<div class="main_txt_down">
-							블루베리스무디 맛있어요.<br />이의이승이 뭐냐구요?<br />일단,
-							와보면 알아요!
-						</div>
-					</div>
-					<!-- 날씨는 반응형 태블릿 때 사라짐 -->
-					<div class="main_wea">
-						<div class="main_wea_up">날씨 위</div>
-						<div class="main_wea_down">날씨 아래</div>
-					</div>
-				</div>
-			</div>
-			<div class="main_2">
-				<div class="main_2_box">
-					<div
-						class="main_box_left"
-						data-aos="fade-down"
-						data-aos-easing="linear"
-						data-aos-duration="1300"
-					>
-						<div class="main_sub">
-							<div class="main_sub_tit">축제정보</div>
-							<div class="main_sub_line"></div>
-						</div>
-						<div class="main_sub_con">
-							<div class="main_sub_txt">
-								경상도에서 인기있는<br />
-								축제를 만나보세요!
-							</div>
-						</div>
-						<div class="main_shap">크리스마스, 우리 함께 즐겨요🎅</div>
-						<!-- 반응형 사라질 때 노란색연필 없애기 -->
-						<img class="main_sub_y" src="/img/yellow.png" alt="pencil" />
-					</div>
-					<div class="main_box_right">
-						<div class="main_2_news">
-							<div class="main_2_news_l">
-								<div class="art_plus">
-									<h3 class="article">경상북도</h3>
-									<a href="#" class="plus_icon"
-										><font-awesome-icon :icon="['fas', 'plus']"
-									/></a>
-								</div>
-								<div class="art_tit">
-									포항 구룡포 선착장에서 열리는 국내 최초의 부두 야시장
-								</div>
-								<div class="art_con">
-									해녀조합과 함께하는 해녀 마켓, 전국 최고의 푸드트럭 만...
-								</div>
-								<p class="art_date">2023.12.01 - 2023.12.10</p>
-							</div>
-							<div class="main_2_news_l">
-								<div class="art_plus">
-									<h3 class="article">경상북도</h3>
-									<a href="#" class="plus_icon"
-										><font-awesome-icon :icon="['fas', 'plus']"
-									/></a>
-								</div>
-								<div class="art_tit">
-									포항 구룡포 선착장에서 열리는 국내 최초의 부두 야시장
-								</div>
-								<div class="art_con">
-									해녀조합과 함께하는 해녀 마켓, 전국 최고의 푸드트럭 만...
-								</div>
-								<p class="art_date">2023.12.01 - 2023.12.10</p>
-							</div>
-							<div class="main_2_news_l">
-								<div class="art_plus">
-									<h3 class="article">경상남도</h3>
-									<a href="#" class="plus_icon"
-										><font-awesome-icon :icon="['fas', 'plus']"
-									/></a>
-								</div>
-								<div class="art_tit">
-									합천 씨파크에서 남녀노소 전 연령대가 다양한 문화를 즐길 수
-									있는 겨울 축제
-								</div>
-								<div class="art_con">
-									실내 난방을 포함, 몸만와서 즐길 수 있는 캠프닉 공간 이...
-								</div>
-								<p class="art_date">2023.12.23 - 2024.02.28</p>
-							</div>
-							<div class="main_2_news_l">
-								<div class="art_plus">
-									<h3 class="article">경상남도</h3>
-									<a href="#" class="plus_icon"
-										><font-awesome-icon :icon="['fas', 'plus']"
-									/></a>
-								</div>
-								<div class="art_tit">
-									합천 씨파크에서 남녀노소 전 연령대가 다양한 문화를 즐길 수
-									있는 겨울 축제
-								</div>
-								<div class="art_con">
-									실내 난방을 포함, 몸만와서 즐길 수 있는 캠프닉 공간 이...
-								</div>
-								<p class="art_date">2023.12.23 - 2024.02.28</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="main_3">
-				<div class="main_3_box">
-					<div
-						class="main_box_left"
-						data-aos="fade-down"
-						data-aos-easing="linear"
-						data-aos-duration="1300"
-					>
-						<div class="main_sub">
-							<div class="main_sub_tit">관광정보</div>
-							<div class="main_sub_line"></div>
-						</div>
-						<div class="main_3_sub_con">
-							<div class="main_3_sub_txt">
-								경상도에서 다양한<br />
-								관광지를 만나보세요!
-							</div>
-						</div>
-						<div class="main_shap">경상도에 이렇게 갈 곳이 많다구요😎</div>
-						<img class="main_3_sub_y" src="/img/yellow.png" alt="pencil" />
-					</div>
-					<div class="main_box_right">
-						<ul class="main_hot3">
+						<a href="#" target="_self">홈이야요</a>
+						<ul class="depth1">
 							<li>
-								<a href="#" target="관광">
-									<div class="main_hot3_img">
-										<img src="/img/topic_1.png" />
-									</div>
-									<div class="main_hot3_txt">
-										<span
-											><font-awesome-icon
-												:icon="['fas', 'circle']"
-												class="icon_cir_1"
-											/>
-											근대역사문화거리</span
-										>
-										<p>
-											100여년 전의 역사가 공존하는<br />
-											특별하고 소중한 시간 여행
-										</p>
-									</div>
-								</a>
+								<a href="#" target="_self" onclick="">블루베리</a>
 							</li>
 							<li>
-								<a href="#" target="여행">
-									<div class="main_hot3_img">
-										<img src="/img/topic_2.png" />
-									</div>
-									<div class="main_hot3_txt">
-										<span
-											><font-awesome-icon
-												:icon="['fas', 'circle']"
-												class="icon_cir_2"
-											/>
-											댕댕이와 경상도여행</span
-										>
-										<p>
-											전용시설에서 반려동물과<br />사계절 뛰어놀 수 있는 특별한
-											곳
-										</p>
-									</div>
-								</a>
-							</li>
-							<li>
-								<a href="#" target="야경">
-									<div class="main_hot3_img">
-										<img src="/img/topic_3.png" />
-									</div>
-									<div class="main_hot3_txt">
-										<span
-											><font-awesome-icon
-												:icon="['fas', 'circle']"
-												class="icon_cir_3"
-											/>
-											낮 보다 아름다운 밤</span
-										>
-										<p>다양한 야간관광 특화도시!<br />경상도의 아름다운 야경</p>
-									</div>
-								</a>
+								<a href="#" target="_self" onclick="">스우므디</a>
 							</li>
 						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="main_4">
-				<div class="main_4_box">
-					<div
-						class="main_box_left"
-						data-aos="fade-down"
-						data-aos-easing="linear"
-						data-aos-duration="1300"
-					>
-						<div class="main_sub">
-							<div class="main_sub_tit">유저광장</div>
-							<div class="main_sub_line"></div>
-						</div>
-						<div class="main_sub_con">
-							<div class="main_sub_txt">
-								유저들과 다양한<br />
-								재미를 공유해봐요!
-							</div>
-						</div>
-						<div class="main_shap">
-							'가가가가?'같은 경상도식 유머가 있어요🤗
-						</div>
-						<img class="main_sub_y" src="/img/yellow.png" alt="pencil" />
-					</div>
-					<div class="main_gg"><img src="/img/com.png" alt="gaga" /></div>
-					<div class="main_box_right">
-						<div class="main_4_menu">
-							<a href="#"
-								>자유<br />게시판<font-awesome-icon :icon="['fas', 'comments']"
-							/></a>
-							<a href="#"
-								>질문<br />게시판<font-awesome-icon :icon="['fas', 'leaf']"
-							/></a>
-							<a href="#"
-								>정보<br />게시판<font-awesome-icon :icon="['fas', 'file-pen']"
-							/></a>
-							<a href="#"
-								>건의<br />게시판<font-awesome-icon
-									:icon="['fas', 'triangle-exclamation']"
-							/></a>
-						</div>
-					</div>
-				</div>
-			</div>
+					</li>
+					<li>
+						<a href="#" target="_self">경상도오</a>
+						<ul class="depth1">
+							<li>
+								<!-- 아예 페이지를 새로 불러옴(created 구문을 실행하기 위해서) -->
+								<a href="#">경상남도구</a>
+							</li>
+							<li>
+								<a href="#">경상북돈디</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#" target="_self">배고프당</a>
+						<ul class="depth1">
+							<li>
+								<a href="#" target="_self" onclick="">마아라탕</a>
+							</li>
+							<li>
+								<a href="#" target="_self" onclick="">먹구시펑</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
 		</div>
-		<div>
-			<footer class="footer">푸터 띄우기</footer>
+		<div class="header_right">
+			<ul class="gnb">
+				<li>
+					<a href="#">로그인</a>
+				</li>
+				<li>
+					<a href="#">회원가입</a>
+				</li>
+			</ul>
 		</div>
+		<!-- <div class="header_right">
+			<ul class="gnb">
+				<li>
+					<div class="header_icon">
+						<div>
+							<font-awesome-icon :icon="['fas', 'circle-user']" />
+						</div>
+					</div>
+				</li>
+				<li>로그아웃</li>
+			</ul>
+		</div> -->
+	</header>
+	<a href="javascript:void(0);" class="menu_btn" @click="toggleMenu">
+		<span></span>
+		<span></span>
+		<span></span>
+		<i class="sound_only">메뉴</i>
+	</a>
+	<div :class="{ ov_hidden: isMenuActive, mo_header: isMenuActive }">
+		<!-- 내용이 들어갈 부분 -->
 	</div>
-	<div class="main_topBtn"></div>
 </template>
 
-<script></script>
+<script>
+export default {
+	name: 'HeaderComponent',
+	data: function () {
+		return {
+			isMenuActive: false,
+			menuItems: [{ active: false, children: [] }],
+		};
+	},
+	methods: {
+		toggleMenu: function () {
+			this.isMenuActive = !this.isMenuActive;
+		},
+		toggleAccordion: function (item) {
+			item.active = !item.active;
+			if (item.active) {
+				this.menuItems.forEach(otherItem => {
+					if (otherItem !== item) {
+						otherItem.active = false;
+					}
+				});
+			}
+		},
+	},
+};
+</script>
 
 <style lang="scss" scoped></style>
