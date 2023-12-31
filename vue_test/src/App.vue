6eb9894a1cbcb00587d9ec229573bbae0daa81d1
mingjicha <mingi9100@naver.com>
2023-12-31 23:22:07 +0900

    1231 scss로 도전한 헤더

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,107 +1,208 @@
 <template>
-	<header class="header">
-		<div class="header_left">
-			<a href="#" class="logo">안냥</a>
-		</div>
-		<div class="header_mid">
-			<nav class="lnb">
-				<ul class="nav">
-					<li>
-						<a href="#" target="_self">홈이야요</a>
-						<ul class="depth1">
-							<li>
-								<a href="#" target="_self" onclick="">블루베리</a>
-							</li>
-							<li>
-								<a href="#" target="_self" onclick="">스우므디</a>
-							</li>
-						</ul>
+	<header id="header" role="banner">
+		<div class="header__inner">
+			<div class="header__logo">
+				<h1>
+					<a href="#">portfolio<em>vue.js</em></a>
+				</h1>
+			</div>
+			<!-- <nav
+				class="header__nav"
+				:class="{ show: isNavVisible }"
+				role="navigation"
+				aria-label="메인 메뉴"
+			> -->
+			<nav class="header__nav" role="navigation" aria-label="메인 메뉴">
+				<!-- <ul>
+          <li v-for="(nav, key) in headerNav" :key="key">
+            <a :href="nav.url" @click="scrollLink($event)">{{ nav.title }}</a>
 					</li>
-					<li>
-						<a href="#" target="_self">경상도오</a>
-						<ul class="depth1">
-							<li>
-								<!-- 아예 페이지를 새로 불러옴(created 구문을 실행하기 위해서) -->
-								<a href="#">경상남도구</a>
-							</li>
-							<li>
-								<a href="#">경상북돈디</a>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a href="#" target="_self">배고프당</a>
-						<ul class="depth1">
-							<li>
-								<a href="#" target="_self" onclick="">마아라탕</a>
-							</li>
-							<li>
-								<a href="#" target="_self" onclick="">먹구시펑</a>
-							</li>
-						</ul>
-					</li>
+				</ul> -->
+				<ul>
+					<li><a href="#intro">intro</a></li>
+					<li><a href="#skill">skill</a></li>
+					<li><a href="#site">site</a></li>
+					<li><a href="#port">portfolio</a></li>
+					<li><a href="#contact">contact</a></li>
 				</ul>
 			</nav>
+			<!-- <div
+      class="header__nav__mobile"
+      id="headerToggle"
+      aria-controls="primary-menu"
+      :aria-expanded="isNavVisible.toString()"
+      role="button"
+      tabindex="0"
+      @click="toggleMobileMenu"
+    > -->
+			<div
+				class="header__nav__mobile"
+				id="headerToggle"
+				aria-controls="primary-menu"
+				aria-expanded="false"
+				role="button"
+				tabindex="0"
+			>
+				<span></span>
+			</div>
 		</div>
-		<div class="header_right">
-			<ul class="gnb">
-				<li>
-					<a href="#">로그인</a>
-				</li>
-				<li>
-					<a href="#">회원가입</a>
-				</li>
-			</ul>
-		</div>
-		<!-- <div class="header_right">
-			<ul class="gnb">
-				<li>
-					<div class="header_icon">
-						<div>
-							<font-awesome-icon :icon="['fas', 'circle-user']" />
-						</div>
-					</div>
-				</li>
-				<li>로그아웃</li>
-			</ul>
-		</div> -->
 	</header>
-	<a href="javascript:void(0);" class="menu_btn" @click="toggleMenu">
-		<span></span>
-		<span></span>
-		<span></span>
-		<i class="sound_only">메뉴</i>
-	</a>
-	<div :class="{ ov_hidden: isMenuActive, mo_header: isMenuActive }">
-		<!-- 내용이 들어갈 부분 -->
-	</div>
 </template>
 
 <script>
 export default {
-	name: 'HeaderComponent',
-	data: function () {
+	data() {
 		return {
-			isMenuActive: false,
-			menuItems: [{ active: false, children: [] }],
+			isNavVisible: false,
 		};
 	},
 	methods: {
-		toggleMenu: function () {
-			this.isMenuActive = !this.isMenuActive;
+		toggleMobileMenu() {
+			this.isNavVisible = !this.isNavVisible;
 		},
-		toggleAccordion: function (item) {
-			item.active = !item.active;
-			if (item.active) {
-				this.menuItems.forEach(otherItem => {
-					if (otherItem !== item) {
-						otherItem.active = false;
-					}
-				});
+		scrollLink(event) {
+			event.preventDefault();
+
+			const targetId = event.target.getAttribute('href');
+			const targetElement = document.querySelector(targetId);
+
+			if (targetElement) {
+				targetElement.scrollIntoView({ behavior: 'smooth' });
 			}
 		},
 	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@import '@/assets/scss/mixin';
+
+#header {
+	@include position-fixed;
+	z-index: 10000;
+}
+.header__inner {
+	@include flex-between;
+	background-color: rgba(116, 99, 99, 0.1);
+	backdrop-filter: blur(15px);
+	padding: 1rem;
+
+	.header__logo {
+		font-size: 0.9rem;
+		text-align: center;
+		text-transform: uppercase;
+		line-height: 1;
+
+		em {
+			font-size: 10px;
+			display: block;
+			color: var(--black200);
+		}
+	}
+
+	.header__nav {
+		@media (max-width: 800px) {
+			display: none;
+
+			&.show {
+				display: block;
+
+				ul {
+					display: block;
+					position: absolute;
+					right: 0;
+					top: 68px;
+					background-color: rgba(116, 99, 99, 0.1);
+					backdrop-filter: blur(15px);
+					z-index: 10000;
+					min-width: 150px;
+					padding: 20px 0;
+
+					li {
+						display: block;
+						text-align: right;
+
+						a {
+							display: inline-block;
+							padding: 10px;
+						}
+					}
+				}
+			}
+			&.show + .header__nav__mobile span::before {
+				width: 20px;
+			}
+			&.show + .header__nav__mobile span::after {
+				width: 20px;
+			}
+		}
+
+		li {
+			display: inline;
+
+			a {
+				text-transform: uppercase;
+				font-size: 14px;
+				padding: 14px;
+				position: relative;
+
+				&::before {
+					content: '';
+					width: calc(100% - 28px);
+					height: 1px;
+					background-color: var(--black);
+					position: absolute;
+					left: 14px;
+					bottom: 10px;
+					transform: scaleX(0);
+					transition: all 0.3s;
+				}
+				&:hover::before {
+					transform: scaleX(1);
+				}
+			}
+		}
+	}
+
+	.header__nav__mobile {
+		display: none;
+		width: 40px;
+		height: 40px;
+		cursor: pointer;
+
+		@media (max-width: 800px) {
+			display: block;
+		}
+
+		span {
+			display: block;
+			width: 40px;
+			height: 2px;
+			background-color: var(--black);
+			margin-top: 19px;
+			position: relative;
+
+			&::before {
+				content: '';
+				width: 40px;
+				height: 2px;
+				background-color: var(--black);
+				position: absolute;
+				right: 0;
+				top: 6px;
+				transition: width 0.3s;
+			}
+			&::after {
+				content: '';
+				width: 40px;
+				height: 2px;
+				background-color: var(--black);
+				position: absolute;
+				left: 0;
+				bottom: 6px;
+				transition: width 0.3s;
+			}
+		}
+	}
+}
+</style>
