9139084c4944adae2b09896a285ceac582c7f884
mingjicha <mingi9100@naver.com>
2024-01-05 17:59:14 +0900

    0105 vue 재정리

--- vue_gym/learn-vue3/src/App.vue
+++ vue_gym/learn-vue3/src/App.vue
@@ -1,54 +1,151 @@
-<!-- 1. npm i swiper -->
 <template>
-	<swiper
-		:slides-per-view="3"
-		:space-between="50"
-		@swiper="onSwiper"
-		@slideChange="onSlideChange"
-	>
-		<swiper-slide>Slide 1</swiper-slide>
-		<swiper-slide>Slide 2</swiper-slide>
-		<swiper-slide>Slide 3</swiper-slide>
-		...
-	</swiper>
 	<div>
-		<!-- header -->
-		<header>헤더</header>
-		<!-- /header -->
-		<div>
-			<main>메인</main>
-		</div>
-		<!-- footer -->
-		<footer>푸터</footer>
-		<!-- /footer -->
+		<nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a
+								class="nav-link dropdown-toggle"
+								href="#"
+								role="button"
+								data-bs-toggle="dropdown"
+								aria-expanded="false"
+							>
+								Dropdown
+							</a>
+							<ul class="dropdown-menu">
+								<li><a class="dropdown-item" href="#">Action</a></li>
+								<li><a class="dropdown-item" href="#">Another action</a></li>
+								<li><hr class="dropdown-divider" /></li>
+								<li>
+									<a class="dropdown-item" href="#">Something else here</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" aria-disabled="true">Disabled</a>
+						</li>
+					</ul>
+					<form class="d-flex" role="search">
+						<input
+							class="form-control me-2"
+							type="search"
+							placeholder="Search"
+							aria-label="Search"
+						/>
+						<button class="btn btn-outline-success" type="submit">
+							Search
+						</button>
+					</form>
+				</div>
+			</div>
+		</nav>
+		<main>
+			<div class="container py-4">
+				<div class="row g-3">
+					<div class="col col-4">
+						<div class="card">
+							<!-- <img src="..." class="card-img-top" alt="..." /> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">
+									Some quick example text to build on the card title and make up
+									the bulk of the card's content.
+								</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col col-4">
+						<div class="card">
+							<!-- <img src="..." class="card-img-top" alt="..." /> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">
+									Some quick example text to build on the card title and make up
+									the bulk of the card's content.
+								</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col col-4">
+						<div class="card">
+							<!-- <img src="..." class="card-img-top" alt="..." /> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">
+									Some quick example text to build on the card title and make up
+									the bulk of the card's content.
+								</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col col-4">
+						<div class="card">
+							<!-- <img src="..." class="card-img-top" alt="..." /> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">
+									Some quick example text to build on the card title and make up
+									the bulk of the card's content.
+								</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col col-4">
+						<div class="card">
+							<!-- <img src="..." class="card-img-top" alt="..." /> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">
+									Some quick example text to build on the card title and make up
+									the bulk of the card's content.
+								</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col col-4">
+						<div class="card">
+							<!-- <img src="..." class="card-img-top" alt="..." /> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">
+									Some quick example text to build on the card title and make up
+									the bulk of the card's content.
+								</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
 	</div>
 </template>
 
 <script>
-// Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue';
-
-// Import Swiper styles
-import 'swiper/css';
-
-export default {
-	components: {
-		Swiper,
-		SwiperSlide,
-	},
-	setup() {
-		const onSwiper = swiper => {
-			console.log(swiper);
-		};
-		const onSlideChange = () => {
-			console.log('slide change');
-		};
-		return {
-			onSwiper,
-			onSlideChange,
-		};
-	},
-};
-</script>
 
 <style lang="scss" scoped></style>
