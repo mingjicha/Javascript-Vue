f4ecc49c467c7b491f7772909330de57f75a5f61
mingjicha <mingi9100@naver.com>
2024-01-06 20:45:00 +0900

    0106 컴포넌트 기초 그리고 Prop

--- vue_gym/learn-vue3/src/main.js
+++ vue_gym/learn-vue3/src/main.js
@@ -1,6 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+// AppCard.vue 전역등록 -> 전역은 어디에서든지 사용할 수 있음 !!
+// import AppCard from './components/AppCard.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+// app.component('AppCard', AppCard);
+
+app.mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
