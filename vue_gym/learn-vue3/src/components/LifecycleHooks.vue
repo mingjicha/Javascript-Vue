666cdd1b781e9eaf294199ab5f83838bcfdd2d84
mingjicha <mingi9100@naver.com>
2024-01-25 23:41:50 +0900

    0125 mounted

--- vue_gym/learn-vue3/src/components/LifecycleHooks.vue
+++ vue_gym/learn-vue3/src/components/LifecycleHooks.vue
@@ -1,19 +1,34 @@
 <template>
-	<div class="container py-4"></div>
+	<div class="container py-4">
+		<input ref="inputRef" type="text" value="hello world!" />
+		<LifecycleChild></LifecycleChild>
+	</div>
 </template>
 
 <script>
+import { onBeforeMount, onMounted, ref } from 'vue';
+import LifecycleChild from './LifecycleChild.vue';
 export default {
+	components: {
+		LifecycleChild,
+	},
 	setup() {
 		console.log('setup');
-		return {};
+		const inputRef = ref(null);
+		onBeforeMount(() => {
+			console.log('onBeforeMount', inputRef.value);
+		});
+		onMounted(() => {
+			console.log('onMounted', inputRef.value.value);
+		});
+		return { inputRef };
 	},
-	beforeCreate() {
-		console.log('beforeCreate');
-	},
-	created() {
-		console.log('created');
-	},
+	// beforeCreate() {
+	// 	console.log('beforeCreate');
+	// },
+	// created() {
+	// 	console.log('created');
+	// },
 };
 </script>
 
