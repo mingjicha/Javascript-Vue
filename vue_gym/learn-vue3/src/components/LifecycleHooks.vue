0fbc7286a1dc2c5c6b267cfcd46358eba182b984
mingjicha <mingi9100@naver.com>
2024-01-26 21:08:16 +0900

    0126 mounted

--- vue_gym/learn-vue3/src/components/LifecycleHooks.vue
+++ vue_gym/learn-vue3/src/components/LifecycleHooks.vue
@@ -1,27 +1,46 @@
 <template>
 	<div class="container py-4">
 		<input ref="inputRef" type="text" value="hello world!" />
-		<LifecycleChild></LifecycleChild>
+		<hr />
+		<button @click="visible = !visible">Toggle Child</button>
+		<LifecycleChild v-if="visible"></LifecycleChild>
+		<p id="message">{{ message }}</p>
 	</div>
 </template>
 
 <script>
-import { onBeforeMount, onMounted, ref } from 'vue';
+import { onBeforeMount, onBeforeUpdate, onMounted, onUpdated, ref } from 'vue';
 import LifecycleChild from './LifecycleChild.vue';
 export default {
 	components: {
 		LifecycleChild,
 	},
 	setup() {
-		console.log('setup');
+		// console.log('setup');
 		const inputRef = ref(null);
-		onBeforeMount(() => {
-			console.log('onBeforeMount', inputRef.value);
-		});
-		onMounted(() => {
-			console.log('onMounted', inputRef.value.value);
-		});
-		return { inputRef };
+		const message = ref('');
+		const visible = ref(false);
+		// onBeforeMount(() => {
+		// 	console.log('onBeforeMount', inputRef.value);
+		// });
+		// onMounted(() => {
+		// 	console.log('onMounted', inputRef.value.value);
+		// });
+		// onBeforeUpdate(() => {
+		// 	console.log('onBeforeUpdate', message.value);
+		// 	console.log(
+		// 		'DOM Content: ',
+		// 		document.querySelector('#message').textContent,
+		// 	);
+		// });
+		// onUpdated(() => {
+		// 	console.log('onUpdated', message.value);
+		// 	console.log(
+		// 		'DOM Content: ',
+		// 		document.querySelector('#message').textContent,
+		// 	);
+		// });
+		return { inputRef, message, visible };
 	},
 	// beforeCreate() {
 	// 	console.log('beforeCreate');
