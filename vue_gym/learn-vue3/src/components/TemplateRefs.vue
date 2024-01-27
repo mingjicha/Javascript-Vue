9a03dbba2e1e6b6877113f86b8c05bb8a38c1bc7
mingjicha <mingi9100@naver.com>
2024-01-27 16:32:46 +0900

    0127 template refs

--- vue_gym/learn-vue3/src/components/TemplateRefs.vue
+++ vue_gym/learn-vue3/src/components/TemplateRefs.vue
@@ -1,25 +1,55 @@
 <template>
 	<div class="container py-4">
+		<!-- 문자열로 할당 -->
 		<input ref="input" type="text" />
 		<p>{{ input }}</p>
 		<!-- input은 mount가 완료될 때까지 null -->
 		<p v-if="input">
 			{{ input.value }}, {{ $refs.input.value }}, {{ $refs.input === input }}
 		</p>
+		<hr />
+		<!-- mount되면 itemRefs 배열에 값이 채워짐 -->
+		<!-- <li v-for="fruit in fruits" :key="fruit" ref="itemRefs">{{ fruit }}</li> -->
+		<!-- el => 머시기 함수로도 할당 -->
+		<!-- <ul>
+			<li
+				v-for="fruit in fruits"
+				:key="fruit"
+				:ref="el => itemRefs.push(el.textContent)"
+			>
+				{{ fruit }}
+			</li>
+		</ul> -->
+		<hr />
+		<TemplateRefsChild ref="child"></TemplateRefsChild>
 	</div>
 </template>
 
 <script>
 import { onMounted, ref } from 'vue';
+import TemplateRefsChild from './TemplateRefsChild.vue';
 export default {
+	components: {
+		TemplateRefsChild,
+	},
 	setup() {
 		const input = ref(null);
 		console.log('onMounted: ', input.value);
 		onMounted(() => {
 			input.value.value = 'Hello World!';
 			console.log('onMounted: ', input.value);
+
+			// itemRefs.value.forEach(item => console.log('item: ', item.textContent));
+			itemRefs.value.forEach(item => console.log('item: ', item));
+			console.log('child.message: ', child.value.message);
+			child.value.sayHello();
 		});
-		return { input };
+
+		const fruits = ref(['사과', '딸기', '포도']);
+		const itemRefs = ref([]);
+
+		const child = ref(null);
+		return { input, fruits, itemRefs, child };
 	},
 };
 </script>
